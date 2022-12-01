8b68a5dc950e9cf685969b688ff06c012c9166b9
alirezaxdehghan <alirezaxdehghan@gmail.com>
2022-12-01 13:51:48 +0100

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Alireza Dehghan on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
